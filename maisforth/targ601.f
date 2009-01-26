314c5a6c54dfb29ea20915a8d7d68fef0cf883e4
hans <hans@09d5f3f9-6417-0410-bd0c-e187ee50e7ab>
2009-01-26 17:58:32 +0000

    checkpoint working maisforth version
    git-svn-id: svn+ssh://netzhansa.com/home/hans/cadr2-svn/trunk/secd@1007 09d5f3f9-6417-0410-bd0c-e187ee50e7ab

--- maisforth/targ601.f
+++ maisforth/targ601.f
@@ -1878,6 +1878,7 @@ CODE COLD ( ? -- )   \ cold start Forth system (AN) 2004
  7F !USART
  CR 0 .MSG
  CR ." Copyright (c) 2005 HCC Forth-gg"
+ CR ." System09 port by Hans Huebner"
  CR HIMEM 0A RSHIFT  9 .R ."  kB RAM"
  CR CR QUIT [
 
@@ -2172,6 +2173,60 @@ ASSEMBLER:
 : REPEAT ( cs: whileadr 6 beginadr 7 -- )   AGAIN THEN ;
 : WHILE  ( cond# cs: adr n -- whileadr 6 adr n )   IF 1 CS-ROLL ;
 : NEXT  Y )++ [] JMP ;   \ 6809 Direct Threaded Code
+
+\ Hans' stuff
+
+\ SECD interface
+EXTRA:
+B140 CONSTANT SECD-STATUS
+B141 CONSTANT SECD-ADDRESS-HIGH
+B200 CONSTANT SECD-RAM-BASE
+
+EXTRA:
+: .SECD-STATUS ( -- )
+    ." SECD: "
+    SECD-STATUS C@
+    DUP 1 AND 0= IF ." RUNNING " ELSE ." STOPPED " THEN
+    1 RSHIFT 3 AND
+    DUP 0 = IF ." RUNNING" THEN
+    DUP 1 = IF ." HALTED" THEN
+    DUP 2 = IF ." GC" THEN
+    3 = IF ." UNKNOWN" THEN
+    CR
+;
+
+: .SECD-PAGE ( N -- )
+    SECD-ADDRESS-HIGH C!
+    BASE @
+    HEX
+    SECD-RAM-BASE 100 DUMP
+    BASE !
+;
+
+: SECD-RAM-TEST ( -- )
+    0 SECD-ADDRESS-HIGH C!
+    HX FF 0 DO
+        I SECD-RAM-BASE I + C!
+    LOOP
+    0 .SECD-PAGE
+;
+
+B0E0 CONSTANT LED
+
+: ON ( N -- )
+    1 SWAP LSHIFT INVERT LED C@ AND LED C! ;
+: OFF ( N -- )
+    1 SWAP LSHIFT LED C@ OR LED C! ;
+
+: NAP 300 MS ;
+
+: LED-DEMO
+    BEGIN
+        5 0 DO  I ON  NAP  LOOP
+        5 0 DO  I OFF NAP  LOOP
+    KEY? UNTIL
+    KEY DROP
+;
 
 \ ----- 20 -----
 \ THROW messages
